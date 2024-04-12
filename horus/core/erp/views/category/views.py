c981e2560cb1240547648c459f23f8c66ff3e4ea
Siegfried16 <imanolrko@gmail.com>
2024-04-11 22:15:52 -0600

    vista basada en clases (ListView)

--- horus/core/erp/views/category/views.py
+++ horus/core/erp/views/category/views.py
@@ -1,11 +1,28 @@
 #vista basada en funcion
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-from core.erp.models import Category
+from core.erp.models import Category, Product
+from django.views.generic import ListView
 
-
+# vistas basadas en funciones
 def category_list(request):
     data = {
         'title': 'Listado de Categorias',
         'categories': Category.objects.all()
     }
     return render(request, 'category/list.html', data)
+
+# vistas basadas en clases
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'category/list.html'
+
+    def get_queryset(self):
+        return Category.objects.filter(name__startswith='L')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Listado de Categorias'
+        #context['object_list'] = Product.objects.all()
+        return context
