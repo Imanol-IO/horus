ab4db7ca1e33dcd1dc472c9552f2a694121985e3
Siegfried16 <imanolrko@gmail.com>
2024-05-08 01:26:33 -0600

    Personalizando nuestro formulario | Parte II

--- horus/horus/settings.py
+++ horus/horus/settings.py
@@ -36,7 +36,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     #Apps
     'core.erp',
-
+    #Libs
+    'widget_tweaks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
